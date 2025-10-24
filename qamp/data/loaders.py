6053a6afde7d8c7f055754fb1f63a4cce0012149
Luke-J-Miller <111100132+Luke-J-Miller@users.noreply.github.com>
2025-10-24 13:37:27 -0500

    Update loaders.py

---
+++ qamp/data/loaders.py
@@ -0,0 +1,7 @@
+from typing import Any, Dict
+
+def load_dataset(cfg: Dict[str, Any]) -> Dict[str, Any]:
+    """
+    Implement in Week 2.
+    """
+    raise NotImplementedError("Implement in Week 2")
