16f867d6bc58bc6098e1e5e0aa67b6db15039f7b
Luke-J-Miller <111100132+Luke-J-Miller@users.noreply.github.com>
2025-11-13 15:31:10 -0600

    Update loaders.py

--- qamp/data/loaders.py
+++ qamp/data/loaders.py
@@ -4,17 +4,68 @@ import gzip
 from pathlib import Path
 from typing import Any#, List, Dict
 
+
+
 def load_benchmark(pkl_path: str | Path) -> dict[int | str, dict[str, Any]]:
-    pkl_path = Path(pkl_path)
+    """
+    Load a QAMP benchmark dataset from a pickle file.
+    
+    Parameters
+    ----------
+    pkl_path : str or Path
+        Path to the benchmark `.pkl` or `.pkl.gz` file. Only recognized
+        benchmark files are allowed. These include:
+        
+        - mutag_benchmark.pkl
+        - snap_benchmark.pkl
+        - ba_benchmark.pkl
+        - er_benchmark.pkl
+    It will also accept any of these ['mutag', 'snap', 'ba', 'er']
+    Returns
+    -------
+    dict[int | str, dict[str, Any]]
+        The benchmark data structure loaded from the pickle file.
+
+    Raises
+    ------
+    FileNotFoundError
+        If the provided path does not exist.
+
+    ValueError
+        If the filename is not in the list of allowed benchmark datasets.
+
+    Notes
+    -----
+    Automatically uses gzip if the file ends with `.gz`.
+    """
+    _ALLOWED_BENCHMARKS = {
+        "mutag_benchmark.pkl",
+        "snap_benchmark.pkl",
+        "ba_benchmark.pkl",
+        "er_benchmark.pkl",
+    }
+    if pkl_path.lower() in ['mutag', 'snap', 'ba', 'er']:
+        match pkl_path.lower():
+            case 'mutag':
+                pkl_path = "mutag_benchmark.pkl"
+            case 'snap':
+                pkl_path = "snap_benchmark.pkl"
+            case 'ba':
+                pkl_path = "ba_benchmark.pkl"
+            case 'er':
+                pkl_path = "er_benchmark.pkl"
+
+
     if not pkl_path.exists():
-        raise FileNotFoundError(pkl_path)
+        
+        raise FileNotFoundError(f"Benchmark file not found: {pkl_path}")
+
 
     opener = gzip.open if pkl_path.suffix == ".gz" else open
     with opener(pkl_path, "rb") as f:
         data: dict[int | str, dict[str, Any]] = pickle.load(f)
+
     return data
-
-
 
 def iter_instances(
     data: dict[int | str, dict[str, Any]],
@@ -72,6 +123,7 @@ def iter_instances(
 
     rng.shuffle(instances)
     return instances
+
 
 
 
