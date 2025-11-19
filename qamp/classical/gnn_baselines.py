ef698d4c22bb0214a0da5c0cb5a6f788e06fb478
Luke-J-Miller <111100132+Luke-J-Miller@users.noreply.github.com>
2025-11-19 11:18:00 -0600

    Update gnn_baselines.py

--- qamp/classical/gnn_baselines.py
+++ qamp/classical/gnn_baselines.py
@@ -38,22 +38,22 @@ class GraphPairBaseline(nn.Module):
         return torch.sigmoid(out)  # probability
 
 
-def adj_mat_to_edge_list(adj_mat):
-    for row in graph:
-        for elem in graph:
-            if elem == 1:
-                if (elem, row) not in edge_tuples:
-                    edge_tuples.append((row, elem))
-    for (u,v) in edge_tuples:
-        graph_edge_index[0].append(u)
-        graph_edge_index[1].append(v)
+def adj_mat_to_edge_list(adj_mat: NDArray) -> torch.Tensor:
+    """
+    Convert adjacency matrix to PyTorch Geometric edge_index (COO)
+    """
+    rows, cols = adj_mat.nonzero()
+    edge_index = torch.tensor([rows, cols], dtype=torch.long)
+    return edge_index
+
     return torch.tensor(graph_edge_index, dtype=torch.long)
 def run_gnn_baseline(graph: NDArray, subgraph: NDArray) -> bool:
+    X_graph = torch.ones((graph.shape[0], 1))
+    X_subgraph = torch.ones((subgraph.shape[0], 1))
     graph_edge_index = adj_mat_to_edge_list(graph)
-    X_graph = graph.shape[0]
     subgraph_edge_index = adj_mat_to_edge_list(subgraph)
-    X_subgraph = subgraph.shape[0]
     model = GraphPairBaseline()
     prob = model(X_graph, graph_edge_index, X_subgraph, subgraph_edge_index)
 
-    return prob > 0.5
+    return (prob.item() > 0.5)
+
